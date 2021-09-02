03a7b35a49737bad21b99e78e9b4227be30d6d91
rafalmeidas <rafalmeidas21@gmail.com>
2021-09-01 22:50:13 -0300

    ADD CMS da prismic.io

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,134 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 import Image from 'next/image';
-
 import techsImage from '../../public/images/techs.svg';
 
-export default function Home() {
-  return (
-   <>
-    <Head>
-      <title>Apaixonado por tecnologia - Sujeito Programador</title>
-    </Head>
-    <main className={styles.container}>
-      <div className={styles.containerHeader}>
-        <section className={styles.ctaText}>
-          <h1>Levando você ao próximo nível!</h1>
-          <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
-          <a>
-            <button>
-              COMEÇAR AGORA!
-            </button>
-          </a>
-        </section>
+import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-        <img 
-            src="/images/banner-conteudos.png" 
-            alt="Conteúdos Sujeito Programador" 
-        />
-      </div>
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileTitle: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+};
 
-      <hr className={styles.divisor} />
-
-      <div className={styles.sectionContent}>
-        <section>
-          <h2>Aprenda criar aplicativos para Android e iOS</h2>
-          <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
-        </section>
-
-        <img src="/images/financasApp.png" alt="Conteúdos desenvolvimento de apps" />
-      </div>
-
-      <hr className={styles.divisor} />
-
-      <div className={styles.sectionContent}>
-        <img src="/images/webDev.png" alt="Conteúdos desenvolvimento de aplicacoes web" />
-
-        <section>
-          <h2>Aprenda criar sistemas web</h2>
-          <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
-        </section>
-      </div>
-
-      <div className={styles.nextLevelContent}>
-        <Image quality={100} src={techsImage} alt="Tecnologias" />
-        <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
-        <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
-        <a>
-          <button>ACESSAR TURMA!</button>
-        </a>
-      </div>
-
-
-
-    </main>
-   </>
-  )
+interface ContentProps {
+  content: Content;
 }
+
+export default function Home({ content }: ContentProps) {
+  return (
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>{content.title}</h1>
+            <span>{content.titleContent}</span>
+            <a href={content.linkAction} target="_blank" rel="noreferrer">
+              <button>COMEÇAR AGORA!</button>
+            </a>
+          </section>
+
+          <img
+            src="/images/banner-conteudos.png"
+            alt="Conteúdos Sujeito Programador"
+          />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>{content.mobileTitle}</h2>
+            <span>{content.mobileContent}</span>
+          </section>
+
+          <img
+            src={content.mobileBanner}
+            alt="Conteúdos desenvolvimento de apps"
+          />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <img
+            src={content.webBanner}
+            alt="Conteúdos desenvolvimento de aplicacoes web"
+          />
+
+          <section>
+            <h2>{content.webTitle}</h2>
+            <span>{content.webContent}</span>
+          </section>
+        </div>
+
+        <div className={styles.nextLevelContent}>
+          <Image quality={100} src={techsImage} alt="Tecnologias" />
+          <h2>
+            Mais de <span className={styles.alunos}>15 mil</span> já levaram sua
+            carreira ao próximo nivel.
+          </h2>
+          <span>
+            E você vai perder a chance de evoluir de uma vez por todas?
+          </span>
+          <a href={content.linkAction} target="_blank" rel="noreferrer">
+            <button>ACESSAR TURMA!</button>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home'),
+  ]);
+
+  const {
+    title,
+    sub_title,
+    link_action,
+    mobile,
+    mobile_content,
+    mobile_banner,
+    title_web,
+    web_content,
+    web_banner,
+  } = response.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url,
+  };
+
+  return {
+    props: {
+      content,
+    },
+    revalidate: 60 * 2, // a cada 2 minutos
+  };
+};
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { GetStaticProps } from 'next';
 
 import {
   FiChevronLeft,
@@ -11,9 +12,26 @@ import {
 } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
-import thumbImg from '../../../public/images/thumb.png';
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { useState } from 'react';
+
+type Post = {
+  slug: string;
+  title: string;
+  description: string;
+  cover: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts: postsBlog }: PostsProps) {
+  const [posts, setPosts] = useState(postsBlog || []);
+
   return (
     <>
       <Head>
@@ -21,25 +39,24 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href="/">
-            <a>
-              <Image
-                src={thumbImg}
-                alt="Post titulo 1"
-                width={720}
-                height={410}
-                quality={100}
-              />
-              <strong>Criando meu primeiro aplicativo</strong>
-              <time>14 HULHO 2021</time>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a{' '}
-              </p>
-            </a>
-          </Link>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a key={post.slug}>
+                <Image
+                  src={post.cover}
+                  alt={post.title}
+                  width={720}
+                  height={410}
+                  quality={100}
+                  placeholder="blur"
+                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOcMGnyfgAF5QJ2NtHMMQAAAABJRU5ErkJggg=="
+                />
+                <strong>{post.title}</strong>
+                <time>{post.updatedAt}</time>
+                <p>{post.description}</p>
+              </a>
+            </Link>
+          ))}
 
           <div className={styles.buttonNavigate}>
             <div>
@@ -65,3 +82,40 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      orderings: '[document.last_publication_date desc]', //ordenar para o mais recente
+      fetch: ['post.title', 'post.description', 'post.cover'],
+      pageSize: 3,
+    }
+  );
+
+  //console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      description:
+        post.data.description.find(content => content.type === 'paragraph')
+          ?.text ?? '',
+      cover: post.data.cover.url,
+      updatedAt: new Date(post.last_publication_date).toLocaleString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60 * 30, //Atualiza a cada 30 minutos
+  };
+};
